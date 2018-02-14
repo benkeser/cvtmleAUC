d1a031170375ab8b37d2b7087f6304e8e9ebf89f
David Benkeser <benkeser@berkeley.edu>
2018-02-13 20:02:37 -0500

    change to elastic net

--- R/wrapper_functions.R
+++ R/wrapper_functions.R
@@ -29,7 +29,7 @@ glm_wrapper <- function(train, test){
 glmnet_wrapper <- function(train, test){
     glmnet_fit <- glmnet::cv.glmnet(x = train$X, y = train$Y,
         lambda = NULL, type.measure = "deviance", nfolds = 5, 
-        family = "binomial", alpha = 0, nlambda = 100)
+        family = "binomial", alpha = 0.5, nlambda = 100)
     Psi_nBn_0 <- function(x){
       stats::predict(glmnet_fit, newx = x, type = "response")
     }
